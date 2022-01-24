880981c8720cb8c3599506d1ff7ef671a0f5cf05
lmwhitehair <lmwhitehair@gmail.com>
2022-01-23 20:23:51 -0500

    Added side navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './header/topbar.component';
+import { SideBarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'github-sidebar',
+    templateUrl: 'sidebar.component.html',
+    styleUrls: ['sidebar.component.css']
+})
+
+export class SideBarComponent {
+
+}
